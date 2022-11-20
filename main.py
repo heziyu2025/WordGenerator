c6accd0b02d173797cc96fabcf6b98c50b52c030
Yuanhui <hyh618@gmail.com>
2022-11-20 13:21:10 +0800

    增加了繁体中文

--- main.py
+++ main.py
@@ -16,13 +16,9 @@ def combine(word):
         r = requests.get(url, params=key_dict)
 
         soup = bs(r.text, 'html.parser')
-        # 查找上图绿色标注标签
         div_content = soup.find("div", {'class': 'content'})
-        # 查找上图橙色标注标签中的a标签
         all_title = div_content.find_all('a', {'data-logid': "ordinal_incgroup_phrases"})
-        # 加入判断 判断all_title是否为空
         if all_title:
-            # 如果all_title不为空
             combine_list = []
             for t in all_title:
                 combine_list.append(t.string)
@@ -35,22 +31,31 @@ def combine(word):
 def zuci_button(self=None):
     output_copy_successed.pack_forget()
     
-    if (len(input_entry.get()) > 1):
-        input_error.config(text='仅支持输入1个汉字！')
+    if len(input_entry.get()) > 1:
+        if language:
+            input_error.config(text='仅支持输入1个汉字！')
+        else:
+            input_error.config(text='僅支持輸入1個漢字！')
         input_error.pack()
         winsound.Beep(440, 500)
         return -1
 
-    if (len(input_entry.get()) == 0):
-        input_error.config(text='输入为空！')
+    if len(input_entry.get()) == 0:
+        if language:
+            input_error.config(text='输入为空！')
+        else:
+            input_error.config(text='輸入為空')
         input_error.pack()
         winsound.Beep(440, 500)
         return -1
 
     output = combine(input_entry.get())
 
-    if (output == -1):
-        input_error.config(text='组词失败！')
+    if output == -1:
+        if language:
+            input_error.config(text='组词失败！')
+        else:
+            input_error.config(text='組詞失敗！')
         input_error.pack()
         winsound.Beep(440, 500)
         return -1
@@ -73,12 +78,52 @@ def output_copy_cmd():
     pyperclip.copy(output_text.get(1.0, tk.END))
     output_copy_successed.pack()
 
+def to_tw():
+    language = False
+    root.title('組詞器')
+    input_frame.config(text='輸入')
+    title.config(text='組詞器')
+    language_menu.config(title='語言')
+    input_frame.config(text='輸入')
+    input_button.config(text='組詞')
+    output_frame.config(text='輸出')
+    output_clear.config(text='清空')
+    output_copy.config(text='複製')
+    statement.config(text='詞語來源：https://guoxue.baike.so.com')
+
+def to_cn():
+    language = True
+    root.title('组词器')
+    input_frame.config(text='输入')
+    title.config(text='组词器')
+    language_menu.config(title='语言')
+    input_frame.config(text='输入')
+    input_button.config(text='组词')
+    output_frame.config(text='输出')
+    output_clear.config(text='清空')
+    output_copy.config(text='复制')
+    statement.config(text='词语来源：https://guoxue.baike.so.com')
+
 root = tk.Tk()
 root.geometry('300x250')
 root.title('组词器')
 root.resizable(False, False)
 
-ttk.Label(root, text='组词器', font=('宋体', 20)).pack()
+title = ttk.Label(root, text='组词器', font=('宋体', 20))
+title.pack()
+
+# Menu
+language = True # true = cn; false = tw
+
+main_menu = tk.Menu(root)
+
+language_menu = tk.Menu(main_menu)
+language_menu.add_radiobutton(label='简体中文', command=to_cn)
+language_menu.add_radiobutton(label='繁體中文', command=to_tw)
+
+main_menu.add_cascade(label='语言', menu=language_menu)
+
+root.config(menu=main_menu)
 
 # 输入部分
 input_frame = ttk.LabelFrame(root, text='输入')
@@ -119,6 +164,7 @@ output_control_frame.pack()
 
 output_frame.pack()
 
-ttk.Label(output_frame, text='词语来源：https://guoxue.baike.so.com').pack()
+statement = ttk.Label(output_frame, text='词语来源：https://guoxue.baike.so.com')
+statement.pack()
 
 root.mainloop()
